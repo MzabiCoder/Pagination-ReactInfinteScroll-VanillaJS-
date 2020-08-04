72b2242f12c71d667937d3d460af4bd9652d8999
Nabil Fannane <nabil@goadroit.com>
2020-08-04 15:00:55 -0700

    last

--- src/App.js
+++ src/App.js
@@ -23,3 +23,64 @@ const App = () => {
  
 
 export default App
+
+// import React,{useEffect,useState} from 'react'
+// import axios from 'axios'
+ 
+// const App = () => {
+
+//   const [results,setdata]=useState([])
+//   const [loading, setLoading] = useState(true)
+//   const [text, setText] = useState(null)
+  
+//   useEffect(() => {
+//     const fetchPeople = async () => {
+//     const {data} = await axios(`https://jsonplaceholder.typicode.com/comments`)
+//       setdata(data)
+//       setLoading(false)
+//     } 
+//     fetchPeople()
+    
+
+//   }, [])
+//   const promt = (name) => {
+    
+//       window.alert(name);
+    
+//   }
+
+//   const change = async e => {
+//     setText(e.target.value)
+     
+//       const {data} = await axios(`https://jsonplaceholder.typicode.com/comments&_comment${text}`)
+//         setdata(data)
+//         setLoading(false)
+       
+//   }
+
+   
+
+//    return (
+//     <div className="wrapper">
+//       <div className="control">
+//          <input type="text" value={text} onChange={change}/>
+//        </div>
+//        {!loading && results.length && results.map(val => (
+//          <div className="list" onClick={()=>promt(val.name)}  style={list} key={val.id}>{val.name}</div>
+//        ))}
+     
+//     </div>
+//   )
+// }
+
+// const list = {
+//   marginTop:'1rem',
+//   padding: '1rem',
+//   backgroundColor: 'grey',
+//   color: 'blue',
+//   cursor:'pointer' 
+// }
+ 
+
+// export default App
+
